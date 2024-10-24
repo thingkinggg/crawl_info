8943602cacacd28d349d27bcad975994cfb66a9a
thingkinggg <55918414+thingkinggg@users.noreply.github.com>
2024-10-25 00:35:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -209,4 +209,5 @@ if 'logged_in' not in st.session_state:
 if not st.session_state.logged_in:
     login()
 else:
+    st.session_state.update_time = "2024년 10월 24일 22:28 업데이트"  # 원하는 시간으로 변경
     main_app()
