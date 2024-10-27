2847c74ec6cbd79f56e8ecbfacfeeaf658cec1c1
thingkinggg <55918414+thingkinggg@users.noreply.github.com>
2024-10-28 00:37:19 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,10 @@ def login():
 
 def main_app():
     st.title("🎈 지자체 크롤링")
-    update_time = st.session_state.get('update_time', "2024년 10월 24일 22:28 업데이트")
+    update_time = st.session_state.get('update_time', "2024년 10월 27일 22:28 업데이트")
     st.write(f"{update_time}\n")
-    st.write("작업진행상황 : 136개 site 최신 1page 수집 작업 완료\n")
-    st.write("향후진행계획 : 나머지 site 최신 페이지 수집, 수집실패사이트점검, 2page이상 수집하도록 변경")
+    st.write("작업진행상황 : 211개 전체 site 최신 1page 수집 작업 완료(1차완료) \n")
+    st.write("향후진행계획 : 수집실패사이트점검, 2page이상 수집하도록 변경")
     
     # 오늘 일자 및 최근 7일 계산
     today = datetime.today()
@@ -162,39 +162,50 @@ def main_app():
     
    # 중간 일배치 수집 로그 텍스트 관리
     log_text = """
-    Processing rows:   0%|          | 0/136 [00:00<?, ?it/s]<ipython-input-1-81f668dc9787>:110: FutureWarning: Setting an item of incompatible dtype is deprecated and will raise in a future error of pandas. Value '2023-03-17' has dtype incompatible with float64, please explicitly cast to a compatible dtype first.
+   Processing rows:   0%|          | 0/211 [00:00<?, ?it/s]<ipython-input-5-c0391dc5c3fd>:112: FutureWarning: Setting an item of incompatible dtype is deprecated and will raise an error in a future version of pandas. Value '2023-03-17' has dtype incompatible with float64, please explicitly cast to a compatible dtype first.
   df.at[index, 'min_date'] = min_date
-<ipython-input-1-81f668dc9787>:111: FutureWarning: Setting an item of incompatible dtype is deprecated and will raise in a future error of pandas. Value '2024-09-03' has dtype incompatible with float64, please explicitly cast to a compatible dtype first.
+<ipython-input-5-c0391dc5c3fd>:113: FutureWarning: Setting an item of incompatible dtype is deprecated and will raise an error in a future version of pandas. Value '2024-09-03' has dtype incompatible with float64, please explicitly cast to a compatible dtype first.
   df.at[index, 'max_date'] = max_date
-Processing rows:  11%|█         | 15/136 [00:22<04:05,  2.03s/it]페이지 로딩 시간이 초과되었습니다: 광주도시관리공사
-Processing rows:  12%|█▏        | 16/136 [01:51<56:28, 28.24s/it]광주도시관리공사페이지 정보를 추출할 수 없습니다.
-Processing rows:  32%|███▏      | 43/136 [04:17<09:38,  6.22s/it]경기도_광명시페이지 정보를 추출할 수 없습니다.
-Processing rows:  35%|███▍      | 47/136 [04:20<03:01,  2.04s/it]요청 오류: 417 Client Error: Expectation Failed for url: https://www.gimpo.go.kr/portal/ntfcPblancList.do?key=1004&cate_cd=1&searchCnd=40900000000
+Processing rows:  20%|██        | 43/211 [04:30<21:53,  7.82s/it]경기도_광명시페이지 정보를 추출할 수 없습니다.
+Processing rows:  22%|██▏       | 47/211 [04:38<08:54,  3.26s/it]요청 오류: 417 Client Error: Expectation Failed for url: https://www.gimpo.go.kr/portal/ntfcPblancList.do?key=1004&cate_cd=1&searchCnd=40900000000
 경기도_김포시페이지 정보를 추출할 수 없습니다.
-Processing rows:  38%|███▊      | 52/136 [05:00<12:00,  8.58s/it]경기도_성남시수정구페이지 정보를 추출할 수 없습니다.
-Processing rows:  41%|████      | 56/136 [05:37<11:21,  8.51s/it]읽기 타임아웃: 경기도_안산시 서버가 데이터를 제공하는 시간이 초과되었습니다.
-경기도_안산시페이지 정보를 추출할 수 없습니다.
-Processing rows:  45%|████▍     | 61/136 [06:13<11:13,  8.98s/it]읽기 타임아웃: 경기도_양주시 서버가 데이터를 제공하는 시간이 초과되었습니다.
-경기도_양주시페이지 정보를 추출할 수 없습니다.
-Processing rows:  48%|████▊     | 65/136 [06:39<09:46,  8.26s/it]연결 타임아웃: 경기도_의정부시 서버로부터 응답이 없습니다.
-경기도_의정부시페이지 정보를 추출할 수 없습니다.
-Processing rows:  79%|███████▉  | 108/136 [12:38<03:43,  8.00s/it]페이지 로딩 시간이 초과되었습니다: 충청도_논산시
-Processing rows:  80%|████████  | 109/136 [13:49<12:03, 26.79s/it]cleaned_dates 리스트가 비어 있습니다.
-Processing rows:  82%|████████▏ | 112/136 [14:25<06:32, 16.37s/it]읽기 타임아웃: 충청도_서산시 서버가 데이터를 제공하는 시간이 초과되었습니다.
-충청도_서산시페이지 정보를 추출할 수 없습니다.
-Processing rows:  84%|████████▍ | 114/136 [14:51<05:20, 14.56s/it]WARNING:urllib3.connection:Failed to parse headers (url=https://www.geumsan.go.kr:443/kr/html/sub03/030302.html): [MissingHeaderBodySeparatorDefect()], unparsed data: 'P3P : CP="NOI CURa ADMa DEVa TAIa OUR DELa BUS IND PHY ONL UNI COM NAV INT DEM PRE"\r\nSet-Cookie: SIDNAME=ronty; path=/; HttpOnly; secure; SameSite=None; secure\r\nCache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0\r\nPragma: no-cache\r\nSet-Cookie: PHPSESSID=8vk0rlp4aurnvl7c1d7a3jnis4; path=/; HttpOnly; secure; SameSite=None\r\nExpires: Thu, 19 Nov 1981 08:52:00 GMT\r\n\r\n'
+Processing rows:  28%|██▊       | 59/211 [06:34<23:57,  9.46s/it]연결 타임아웃: 경기도_안성시 서버로부터 응답이 없습니다.
+경기도_안성시페이지 정보를 추출할 수 없습니다.
+Processing rows:  32%|███▏      | 67/211 [07:48<21:52,  9.12s/it]페이지 로딩 시간이 초과되었습니다: 경기도_평택시
+Processing rows:  43%|████▎     | 90/211 [12:09<17:02,  8.45s/it]읽기 타임아웃: 강원도_홍천군 서버가 데이터를 제공하는 시간이 초과되었습니다.
+강원도_홍천군페이지 정보를 추출할 수 없습니다.
+Processing rows:  54%|█████▍    | 114/211 [17:42<22:17, 13.79s/it]WARNING:urllib3.connection:Failed to parse headers (url=https://www.geumsan.go.kr:443/kr/html/sub03/030302.html): [MissingHeaderBodySeparatorDefect()], unparsed data: 'P3P : CP="NOI CURa ADMa DEVa TAIa OUR DELa BUS IND PHY ONL UNI COM NAV INT DEM PRE"\r\nSet-Cookie: SIDNAME=ronty; path=/; HttpOnly; secure; SameSite=None; secure\r\nCache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0\r\nPragma: no-cache\r\nSet-Cookie: PHPSESSID=vl6re9loeqlon179jjbui0o2e4; path=/; HttpOnly; secure; SameSite=None\r\nExpires: Thu, 19 Nov 1981 08:52:00 GMT\r\n\r\n'
 Traceback (most recent call last):
   File "/usr/local/lib/python3.10/dist-packages/urllib3/connection.py", line 510, in getresponse
     assert_header_parsing(httplib_response.msg)
   File "/usr/local/lib/python3.10/dist-packages/urllib3/util/response.py", line 88, in assert_header_parsing
     raise HeaderParsingError(defects=defects, unparsed_data=unparsed_data)
-urllib3.exceptions.HeaderParsingError: [MissingHeaderBodySeparatorDefect()], unparsed data: 'P3P : CP="NOI CURa ADMa DEVa TAIa OUR DELa BUS IND PHY ONL UNI COM NAV INT DEM PRE"\r\nSet-Cookie: SIDNAME=ronty; path=/; HttpOnly; secure; SameSite=None; secure\r\nCache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0\r\nPragma: no-cache\r\nSet-Cookie: PHPSESSID=8vk0rlp4aurnvl7c1d7a3jnis4; path=/; HttpOnly; secure; SameSite=None\r\nExpires: Thu, 19 Nov 1981 08:52:00 GMT\r\n\r\n'
-Processing rows:  88%|████████▊ | 119/136 [16:00<04:02, 14.25s/it]충청도_청양군페이지 정보를 추출할 수 없습니다.
-Processing rows:  93%|█████████▎| 126/136 [17:20<02:07, 12.74s/it]연결 타임아웃: 전라도_전주시 서버로부터 응답이 없습니다.
+urllib3.exceptions.HeaderParsingError: [MissingHeaderBodySeparatorDefect()], unparsed data: 'P3P : CP="NOI CURa ADMa DEVa TAIa OUR DELa BUS IND PHY ONL UNI COM NAV INT DEM PRE"\r\nSet-Cookie: SIDNAME=ronty; path=/; HttpOnly; secure; SameSite=None; secure\r\nCache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0\r\nPragma: no-cache\r\nSet-Cookie: PHPSESSID=vl6re9loeqlon179jjbui0o2e4; path=/; HttpOnly; secure; SameSite=None\r\nExpires: Thu, 19 Nov 1981 08:52:00 GMT\r\n\r\n'
+Processing rows:  58%|█████▊    | 122/211 [19:15<18:59, 12.80s/it]전라도_군산시페이지 정보를 추출할 수 없습니다.
+Processing rows:  59%|█████▉    | 124/211 [19:20<10:48,  7.46s/it]요청 오류: 400 Client Error: Bad Request for url: https://www.namwon.go.kr/board/post/list.do?boardUid=ff8080818ea1fec5018ea24137680031&menuUid=ff8080818e3beff0018e4077131b007a&beginDateStr=&endDateStr=&searchType=postTtl&keyword=%EC%A0%9C%EC%95%88&paramString=Us7WVBAxc13kgzv1JU3ayAslPphGSM%2FmlfdB4qtWC4OBeJsElaKmGl7kvQ4Au%2B3O&size=10
+전라도_남원시페이지 정보를 추출할 수 없습니다.
+Processing rows:  60%|█████▉    | 126/211 [20:11<22:20, 15.77s/it]연결 타임아웃: 전라도_전주시 서버로부터 응답이 없습니다.
 전라도_전주시페이지 정보를 추출할 수 없습니다.
-Processing rows:  95%|█████████▍| 129/136 [17:50<01:15, 10.72s/it]cleaned_dates 리스트가 비어 있습니다.
-Processing rows:  98%|█████████▊| 133/136 [18:36<00:37, 12.62s/it]전라도_임실군페이지 정보를 추출할 수 없습니다.
-Processing rows: 100%|██████████| 136/136 [19:06<00:00,  8.43s/it]
+Processing rows:  61%|██████    | 129/211 [20:50<19:10, 14.03s/it]cleaned_dates 리스트가 비어 있습니다.
+Processing rows:  63%|██████▎   | 133/211 [21:45<19:55, 15.33s/it]전라도_임실군페이지 정보를 추출할 수 없습니다.
+Processing rows:  66%|██████▋   | 140/211 [23:12<11:43,  9.91s/it]요청 오류: ('Received response with content-encoding: gzip, but failed to decode it.', error('Error -3 while decompressing data: incorrect data check'))
+전라도_여수시페이지 정보를 추출할 수 없습니다.
+Processing rows:  69%|██████▉   | 146/211 [24:04<09:35,  8.86s/it]요청 오류: ('Received response with content-encoding: gzip, but failed to decode it.', error('Error -3 while decompressing data: incorrect data check'))
+전라도_무안군페이지 정보를 추출할 수 없습니다.
+Processing rows:  70%|██████▉   | 147/211 [24:14<10:00,  9.38s/it]읽기 타임아웃: 전라도_보성군 서버가 데이터를 제공하는 시간이 초과되었습니다.
+전라도_보성군페이지 정보를 추출할 수 없습니다.
+Processing rows:  74%|███████▍  | 157/211 [25:17<07:09,  7.95s/it]전라도_화순군페이지 정보를 추출할 수 없습니다.
+Processing rows:  77%|███████▋  | 162/211 [25:50<05:09,  6.31s/it]요청 오류: 404 Client Error: Not Found for url: https://www.gbmg.go.kr/portal/saeol/gosi/list.do?mId=0301060000
+경상도_문경시페이지 정보를 추출할 수 없습니다.
+Processing rows:  79%|███████▊  | 166/211 [26:01<02:23,  3.19s/it]경상도_영천시페이지 정보를 추출할 수 없습니다.
+Processing rows:  81%|████████  | 170/211 [26:37<05:05,  7.45s/it]cleaned_dates 리스트가 비어 있습니다.
+Processing rows:  81%|████████  | 171/211 [26:49<05:49,  8.73s/it]cleaned_dates 리스트가 비어 있습니다.
+Processing rows:  82%|████████▏ | 173/211 [27:03<05:11,  8.21s/it]cleaned_dates 리스트가 비어 있습니다.
+Processing rows:  84%|████████▍ | 177/211 [27:51<06:17, 11.09s/it]cleaned_dates 리스트가 비어 있습니다.
+Processing rows:  84%|████████▍ | 178/211 [28:04<06:28, 11.76s/it]경상도_청송군페이지 정보를 추출할 수 없습니다.
+Processing rows:  91%|█████████▏| 193/211 [28:47<00:57,  3.21s/it]요청 오류: HTTPSConnectionPool(host='www.namhae.go.kr', port=443): Max retries exceeded with url: /modules/saeol/gosi.do?dep_nm=%EB%82%A8%ED%95%B4%EC%9D%8D&pageCd=WW0512010103&siteGubun=portal (Caused by NameResolutionError("<urllib3.connection.HTTPSConnection object at 0x7cf3b8176020>: Failed to resolve 'www.namhae.go.kr' ([Errno -2] Name or service not known)"))
+경상도_남해군_남해읍페이지 정보를 추출할 수 없습니다.
+Processing rows: 100%|██████████| 211/211 [29:58<00:00,  8.53s/it]
     """
     
 
@@ -209,5 +220,5 @@ if 'logged_in' not in st.session_state:
 if not st.session_state.logged_in:
     login()
 else:
-    st.session_state.update_time = "2024년 10월 24일 22:28 업데이트"  # 원하는 시간으로 변경
+    st.session_state.update_time = "2024년 10월 27일 22:28 업데이트"  # 원하는 시간으로 변경
     main_app()
