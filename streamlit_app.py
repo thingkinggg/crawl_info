ed7272496358bc553f7a6d58226e0e0e936739f6
thingkinggg <55918414+thingkinggg@users.noreply.github.com>
2024-11-11 18:06:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ def login():
         if password == PASSWORD:
             st.session_state.logged_in = True
             st.success("로그인 성공!")   
-            st.experimental_rerun()
+            st.rerun()
         else:
             st.error("비밀번호가 올바르지 않습니다.")
 
