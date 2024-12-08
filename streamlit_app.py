a8e691bdc8fbc4c72d497743db2c538f2884e2e6
thingkinggg <55918414+thingkinggg@users.noreply.github.com>
2024-12-08 23:48:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,7 @@ def login():
 def main_app():
     st.title("🎈 지자체 크롤링")
 
-    st.write(" 2024년 12월 08일 11:79 업데이트")
-   
+    st.write("2024년 12월 08일 11:79 업데이트")   
     st.write("문의 있으실 경우 deepbid2024@gmail.com 으로 연락부탁드립니다.")
     
     # 버튼 클릭 시 Google 스프레드시트로 이동
